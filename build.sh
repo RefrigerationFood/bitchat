793ae15d82b3543289e4cda25ffdf0f640135b36
punkproger <punkproger@gmail.com>
2020-01-02 15:27:54 +0200

    Added --clean option for build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@
 BUILD_DIR_NAME="CMakeOutput"
 SCRIPT_PATH=$(pwd)
 
+if [[ $* == *--clean* ]]; then
+  echo "*** CLEAN BUILD ***"
+  rm -rf ${BUILD_DIR_NAME}
+fi
+
 if [ ! -d "${BUILD_DIR_NAME}" ]; then
   # Control will enter here if ${BUILD_DIR_NAME} doesn't exist.
+  echo "*** MAKE DIR ${BUILD_DIR_NAME} ***"
   mkdir ${BUILD_DIR_NAME}
 fi
 cd ${BUILD_DIR_NAME}
